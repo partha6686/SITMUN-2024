7460cd1d08ac71ec706b40f1604357407391bb1d
Partha Sarathi Praharaj <parthasarathipraharaj@Parthas-MacBook-Pro.local>
2023-12-25 02:18:59 +0530

    Updated file folder structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/shared/Navbar'
 import Hero from '../components/Home/Hero'
 import Gallery from '../components/Home/Gallery'
 import Footer from '../components/shared/Footer'
+import About from '../components/Home/About'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
       <Gallery />
       <Footer />
     </div>
