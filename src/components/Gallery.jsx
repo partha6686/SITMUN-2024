984a339eedfe6d4224da24d9a59efc5d6b2aa4a0
ArpitaBeheragit <eee.21beee39@gmail.com>
2023-12-30 22:44:05 +0530

    gallery section

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import styles from '../pages/Gallery.module.css';
+import ReactDOM from 'react-dom';
+import ReactImageZoom from 'react-image-zoom';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const [galleryData, setGalleryData] = useState({ gallery1: [], gallery2: [] });
 
-export default Gallery
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 12000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 12000,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const settings1 = {
+    dots: false,
+    infinite: true,
+    speed: 12000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 12000,
+    
+    rtl:true,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res1 = await fetch("http://localhost:4000/gallery1");
+        const res2 = await fetch("http://localhost:4000/gallery2");
+        const data1 = await res1.json();
+        const data2 = await res2.json();
+        setGalleryData({ gallery1: data1, gallery2: data2 });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  return (
+    <div>
+      
+        <div >
+        <Slider {...settings} className={styles.slider}>
+          {galleryData.gallery1.map((item) => (
+            <div key={item.id} className={styles.sliderItem}>
+              <img src={item.url} alt={`Image ${item.id}`} className={styles.sliderimg}/>
+            </div>
+          ))}
+          </Slider>
+          <Slider {...settings1} className={styles.slider1}>
+          {galleryData.gallery2.map((item) => (
+            <div key={item.id} className={styles.sliderItem}>
+              <img src={item.url} alt={`Image ${item.id}`} className={styles.sliderimg}/>
+            </div>
+          ))}
+          </Slider>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Gallery;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/shared/Navbar'
 import Hero from '../components/Hero'
 import Gallery from '../components/Gallery'
 import Footer from '../components/shared/Footer'
+// import Countdown from '../components/Countdown'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      {/* <Countdown/> */}
       <Gallery />
       <Footer />
     </div>
