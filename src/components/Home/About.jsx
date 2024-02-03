81808b9bbe1c78f6efe330415dcd7a06f2e626c6
ankitBytes <sahuankitkumar99@gmail.com>
2024-02-03 21:14:52 +0530

    updated

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from "react";
 import {
-  Button,
-  Card,
-  CardContent,
   Typography,
-  Grid,
-  Container,
+  Box,
+  Stack
 } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 // import SwiperCore, { EffectCards, Mousewheel } from "swiper";
+import Heading from "../shared/Heading"
 
 // import Swiper and modules styles
 import "swiper/css";
@@ -24,20 +22,23 @@ import "../../styles/about.css";
 const About = () => {
   return (
     <section>
-      <div className="content">
-        <div className="info">
-          <p>
+      <Stack className="content" flexDirection={{ md: 'row', xs: 'column-reverse'}}>
+        <Box className="info">
+          <Heading
+            heading={"About us"}
+          />
+          <Typography variant="body1">
             <span className="movie-night">Model United Nations</span> is a simulation that imitates the procedures of the United Nations finding solutions for real-world problems, done through an academic simulation of the United Nations procedure. The MUN simulation consists of negotiation, discussion, and lively debates, which is the cornerstone of UN activity. Model UN aims to educate students about current events, international relations, diplomacy, and the United Nations agenda. Along with knowledge of the world we live in, participants develop skills including public speaking, presentation, persuasion, situation analysis, research, and critical thinking. SITMUN is an inter-collegiate conference conducted every year. Students from SiliconTech and other institutes across Odisha, participate as delegates in different committees. From each committee the best speakers are awarded.
-          </p>
+          </Typography>
           {/* <button className="btn">Join</button> */}
-        </div>
+        </Box>
         <Swiper className="swiper">
           <div className="swiper-wrapper">
             <SwiperSlide className="swiper-slide">
             </SwiperSlide>
           </div>
         </Swiper>
-      </div>
+      </Stack>
 
       <ul className="circles">
         <li></li>
