1e6d3fd70a4d538f01b27428bbe7ebacece1d135
ArpitaBeheragit <eee.21beee39@gmail.com>
2024-02-09 20:48:08 +0530

    gallery dasta added

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -39,9 +39,7 @@ export const galleryData = [
     {
         url: "https://ik.imagekit.io/0oohdkbtxa/_DSC0913.jpg?updatedAt=1707487601886"
     },
-    {
-        url: "https://ik.imagekit.io/0oohdkbtxa/IMG_4525.jpg?updatedAt=1707487916826"
-    },
+    
     {url:"https://ik.imagekit.io/0oohdkbtxa/IMG_4559.jpg?updatedAt=1707487917154" },
         {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_2960.jpg?updatedAt=1707488210563"},
         {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_3497.jpg?updatedAt=1707489278803"},
@@ -49,6 +47,16 @@ export const galleryData = [
         {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_3473.jpg?updatedAt=1707488970304"},
         {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_0167.jpg?updatedAt=1707488631197"},
         
-        {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_3277.jpg?updatedAt=1707488830816"}
+        {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_3277.jpg?updatedAt=1707488830816"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/WhatsApp%20Image%202024-02-01%20at%2023.18.37.jpeg?updatedAt=1707485163977"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/IMG_7220.jpg?updatedAt=1707491017433"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_3276.jpg?updatedAt=1707491018772"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/DSC_3363.jpg?updatedAt=1707491019019"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/IMG_7422.jpg?updatedAt=1707491019827"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/IMG_7159.jpg?updatedAt=1707491021186"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/IMG_8535.jpg?updatedAt=1707491428293"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/WhatsApp%20Image%202024-02-01%20at%2023.10.05.jpeg?updatedAt=1707491676727"},
+        {url:"https://ik.imagekit.io/0oohdkbtxa/IMG_8443.jpg?updatedAt=1707491830834"}
+        
     
 ]
