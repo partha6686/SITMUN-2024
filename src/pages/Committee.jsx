f55c6d09a21aff46433442df7c49500dd807cbe8
Partha <parthasarathi6686@gmail.com>
2024-02-13 02:07:40 +0530

    add committees page

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Hero from '../components/Home/Hero'
+import CommitteesSlider from '../components/Committee/CommitteesSlider'
 
 const Committee = () => {
   return (
+    <>
     <Hero />
+    <CommitteesSlider />
+    </>
   )
 }
 
