d446a0407adfcb350489fdac77df7a73df88747a
Partha <parthasarathi6686@gmail.com>
2024-02-17 13:38:48 +0530

    vercel bug fix and lazy loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../components/shared/Navbar";
 import Hero from "../components/Home/Hero";
+import Footer from "../components/shared/Footer";
+import CalcTimeDelta from "../components/Countdown";
+import About from "../components/Home/About";
+import TimelineView from "../components/Home/Timeline";
+import Message from "../components/Home/Message";
+import Faq from "../components/Home/Faq";
+
 const Gallery = React.lazy(() => import("../components/Home/Gallery"));
-const Footer = React.lazy(()=> import("../components/shared/Footer"));
-const CalcTimeDelta = React.lazy(()=> import("../components/Countdown"));
 const Feedback = React.lazy(()=> import("../components/Feedback"));
-const About = React.lazy(()=> import("../components/Home/About"));
-const TimelineView = React.lazy(()=> import("../components/Home/Timeline"));
-const Message = React.lazy(()=> import("../components/Home/Message"));
-const Faq = React.lazy(()=> import("../components/Home/Faq"));
 
 import "../styles/circles.css"
 
