343ed74572bf552263eb2c1fb69d85f36a214f69
ankitBytes <sahuankitkumar99@gmail.com>
2024-02-25 13:41:21 +0530

    close button added

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -5,9 +5,11 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { feedbackdata } from "../data/feedback";
 import Heading from "./shared/Heading";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import CloseIcon from "@mui/icons-material/Close";
+import { Button } from "@mui/material";
 
 const Feedback = () => {
   const sliderRef = useRef(null);
@@ -24,16 +26,22 @@ const Feedback = () => {
   };
 
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 500,
-    maxWidth: { xs: 300, sm:500,md: 500 },
-    bgcolor: '#313131',
-    border: '2px solid #000',
+    maxWidth: { xs: 300, sm: 500, md: 500 },
+    bgcolor: "#0148006a",
+    backdropFilter: 'blur(30px)',
+    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
+    maxHeight: '70vh',
+    overflowY: {sm: 'hidden', xs: 'scroll'},
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end'
   };
 
   const settings = {
@@ -82,17 +90,15 @@ const Feedback = () => {
           <div key={id} className={styles.testimonial}>
             <div className={styles.testimonial_bubble}>
               <div className={styles.quotes}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
-
-  <path
-    fill="currentColor" 
-    d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621c.537-.278 1.24-.375 1.929-.311c1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5a3.871 3.871 0 0 1-2.748-1.179m10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621c.537-.278 1.24-.375 1.929-.311c1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5a3.871 3.871 0 0 1-2.748-1.179"
-    style={{
-      color:"rgb(10 59 10)"
-    }}
-  />
-</svg>
-
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
+                  <path
+                    fill="currentColor"
+                    d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621c.537-.278 1.24-.375 1.929-.311c1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5a3.871 3.871 0 0 1-2.748-1.179m10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621c.537-.278 1.24-.375 1.929-.311c1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5a3.871 3.871 0 0 1-2.748-1.179"
+                    style={{
+                      color: "rgb(10 59 10)",
+                    }}
+                  />
+                </svg>
               </div>
               <div className={styles.testimonial_text}>
                 <p>
@@ -104,7 +110,10 @@ const Feedback = () => {
                           <a
                             href="#"
                             onClick={() => openModal(item)}
-                            style={{ color: "#4E9F3D", textDecoration: "underline" }}
+                            style={{
+                              color: "#4E9F3D",
+                              textDecoration: "underline",
+                            }}
                           >
                             &nbsp;See More
                           </a>
@@ -115,7 +124,10 @@ const Feedback = () => {
                           <a
                             href="#"
                             onClick={() => openModal(item)}
-                            style={{ color: "#4E9F3D", textDecoration: "underline" }}
+                            style={{
+                              color: "#4E9F3D",
+                              textDecoration: "underline",
+                            }}
                           >
                             &nbsp;See More
                           </a>
@@ -144,10 +156,12 @@ const Feedback = () => {
         onClose={closeModal}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-        
       >
-        <Box sx={style} >
+        <Box sx={style}>
           {/* <img style={{width:"100%", maxHeight: "200px", objectFit: "cover"}} src={selectedFeedback && selectedFeedback.url} alt={`Image`} /> */}
+          <Button onClick={closeModal} sx={{ margin: '1rem 0'}}>
+            <CloseIcon sx={{ color: 'white'}} />
+          </Button>
           <Typography
             sx={{ color: "white", overflowWrap: "break-word" }}
             variant="body1"
