dd5f50d9fb86cea8c3ebf9fed226465e79baa7a6
ArpitaBeheragit <eee.21beee39@gmail.com>
2024-02-27 22:41:29 +0530

    spacing added

--- src/components/Home/Message.jsx
+++ src/components/Home/Message.jsx
@@ -10,60 +10,60 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const SecGenMsg = () => {
-  return (
-    <div className={styles.mainContainer}>
-      <div className={styles.textContainer}>
-        <div className={styles.heading}>
-          <Heading heading={"Message From secretary general"} />
-        </div>
-        <Grid container>
-          <Grid item xs={6} sm={6} className={styles.content} fontFamily="comfortaa,cursive">
-            <p>
-              "My name is Namrata Mishra, and it is with distinct honour that I
-              welcome you all to the 6th iteration of the SIT Model United
-              Nations conference! I have been a part of SITMUN as a Delegate,
-              Coordinator, and OC Member, and it is now my privilege to lead the
-              Secretariat as Secretary General for the conference. With my
-              incredible team, I am confident that this year’s conference will
-              surpass all expectations for SITMUN and redefine what our
-              conference has historically delivered. SITMUN's mission is simple;
-              we strive to provide young leaders a stimulating platform to
-              debate, discuss, and resolve critical global issues by promoting
-              equity, creativity, and diplomacy. At SITMUN, Model United Nations
-              is more than a simulation of the UN, but rather, it is a space for
-              delegates to delve into the world of international issues and
-              develop stronger public speaking, communication, critical
-              thinking, and diplomacy skills. Whether you are the most
-              passionate and knowledgeable political thinker or, perhaps, are
-              someone foreign to the world of diplomacy, SITMUN is for you. To
-              my delegates— SITMUN is your space to collaborate, engage and
-              debate the world of today, to lead the world of tomorrow. The
-              legacy of SITMUN is established by a long line of predecessors who
-              have established our conference as one of the best in the circuit.
-              This year is no exception, as the 6th iteration of SITMUN could
-              not be possible without the contributions of my hard-working,
-              passionate, and dedicated Secretariat and OC members. They are,
-              genuinely, one of the most brilliant individuals I know, and I
-              have no doubt that their talents and efforts will reflect in your
-              SITMUN experience. I am thrilled to see what SITMUN 2024 will
-              bring, and I am incredibly excited to welcome you all to the
-              conference this March."
-            </p>
-          </Grid>
-          <Grid item xs={6} sm={6} className={styles.imageContainer}>
-            {/* <div className={styles.signature}>
-              <div className={styles.dance}>Namrata Mishra</div>
-              <div className={styles.sec}>
-                <b>Secretary General,SITMUN 2024</b>
-              </div>
-            </div> */}
-          </Grid>
-        </Grid>
-      </div>
-    </div>
-  );
-};
+// const SecGenMsg = () => {
+//   return (
+//     <div className={styles.mainContainer}>
+//       <div className={styles.textContainer}>
+//         <div className={styles.heading}>
+//           <Heading heading={"Message From secretary general"} />
+//         </div>
+//         <Grid container>
+//           <Grid item xs={6} sm={6} className={styles.content} fontFamily="comfortaa,cursive">
+//             <p>
+//               "My name is Namrata Mishra, and it is with distinct honour that I
+//               welcome you all to the 6th iteration of the SIT Model United
+//               Nations conference! I have been a part of SITMUN as a Delegate,
+//               Coordinator, and OC Member, and it is now my privilege to lead the
+//               Secretariat as Secretary General for the conference. With my
+//               incredible team, I am confident that this year’s conference will
+//               surpass all expectations for SITMUN and redefine what our
+//               conference has historically delivered. SITMUN's mission is simple;
+//               we strive to provide young leaders a stimulating platform to
+//               debate, discuss, and resolve critical global issues by promoting
+//               equity, creativity, and diplomacy. At SITMUN, Model United Nations
+//               is more than a simulation of the UN, but rather, it is a space for
+//               delegates to delve into the world of international issues and
+//               develop stronger public speaking, communication, critical
+//               thinking, and diplomacy skills. Whether you are the most
+//               passionate and knowledgeable political thinker or, perhaps, are
+//               someone foreign to the world of diplomacy, SITMUN is for you. To
+//               my delegates— SITMUN is your space to collaborate, engage and
+//               debate the world of today, to lead the world of tomorrow. The
+//               legacy of SITMUN is established by a long line of predecessors who
+//               have established our conference as one of the best in the circuit.
+//               This year is no exception, as the 6th iteration of SITMUN could
+//               not be possible without the contributions of my hard-working,
+//               passionate, and dedicated Secretariat and OC members. They are,
+//               genuinely, one of the most brilliant individuals I know, and I
+//               have no doubt that their talents and efforts will reflect in your
+//               SITMUN experience. I am thrilled to see what SITMUN 2024 will
+//               bring, and I am incredibly excited to welcome you all to the
+//               conference this March."
+//             </p>
+//           </Grid>
+//           <Grid item xs={6} sm={6} className={styles.imageContainer}>
+//             {/* <div className={styles.signature}>
+//               <div className={styles.dance}>Namrata Mishra</div>
+//               <div className={styles.sec}>
+//                 <b>Secretary General,SITMUN 2024</b>
+//               </div>
+//             </div> */}
+//           </Grid>
+//         </Grid>
+//       </div>
+//     </div>
+//   );
+// };
 
 const Message = () => {
   return (
@@ -101,9 +101,11 @@ const Message = () => {
           xs={12}
           sx={{
             padding: "3vh 0",
+            
           }}
+          
         >
-          <Heading heading={"Message From Secretary General"} />
+          <Heading heading={"Message From Secretary\n General"} />
         </Grid>
           <Grid
             item
